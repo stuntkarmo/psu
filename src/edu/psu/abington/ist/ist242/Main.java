d19f0022f79d5c47a3e2fdc9f5afedeeb20d2c2a
stuntkarmo <stuntkarmo@gmail.com>
2020-03-21 10:21:27 -0400

    Edit

---
+++ src/edu/psu/abington/ist/ist242/Main.java
@@ -0,0 +1,17 @@
+/*
+Project: Hello World
+Purpose Details: To learn how to print to the console using Java
+Course:IST 242
+Author: Karmo Senah
+Date Developed: 1/18/2020
+Last Date Changed: 1/19/2020
+Revision: 1
+*/
+package edu.psu.abington.ist.ist242;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World Senah");
+    }
+}
